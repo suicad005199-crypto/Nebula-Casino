e82a4285ef2a2b5ba75971504af55bed7c3d048a
suicad005199-crypto <suicad005199@gmail.com>
2026-03-09 13:05:37 +0800

    Update lobby.js

--- assets/js/lobby.js
+++ assets/js/lobby.js
@@ -1,1 +1,13 @@
+import { Games } from './games.js';
+import { Utils } from './utils.js';
 
+export async function initLobby() {
+    const list = await Games.getList();
+    const grid = document.getElementById('game-grid');
+    grid.innerHTML = list.map(g => `
+        <div class="game-card">
+            <div class="game-img" style="background-image:url('/assets/images/games/${g.img}')"></div>
+            <div style="padding:8px; font-size:12px;">${g.title}</div>
+        </div>
+    `).join('');
+}
