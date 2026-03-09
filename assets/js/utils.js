e9a2c795093848602a735ea4048e4dea92fa7a40
suicad005199-crypto <suicad005199@gmail.com>
2026-03-09 13:04:54 +0800

    Update utils.js

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,1 +1,5 @@
-
+export const Utils = {
+    formatCurrency: (num) => new Intl.NumberFormat('zh-TW', { minimumFractionDigits: 2 }).format(num),
+    showLoading: () => console.log('Loading...'),
+    hideLoading: () => console.log('Done.')
+};
