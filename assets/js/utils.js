75dfd5bef8b4d286ae0d880607b9cf1d8fc81240
suicad005199-crypto <suicad005199@gmail.com>
2026-03-09 13:45:01 +0800

    Update utils.js

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,5 +1,2 @@
-export const Utils = {
-    formatCurrency: (num) => new Intl.NumberFormat('zh-TW', { minimumFractionDigits: 2 }).format(num),
-    showLoading: () => console.log('Loading...'),
-    hideLoading: () => console.log('Done.')
-};
+export const formatCurrency = (val) => new Intl.NumberFormat('zh-TW').format(val);
+export const showToast = (msg) => alert(msg); // 可改為 iOS Style Toast
